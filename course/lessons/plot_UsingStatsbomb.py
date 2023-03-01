03fc35af6e2547ddd3e0dc05ec8a70226eab6d2a
OJ <o.j.uwaifo@gmail.com>
2023-03-01 11:17:16 +0000

    refactor and add comments to plot_UsingStatsbomb.py

--- course/lessons/plot_UsingStatsbomb.py
+++ course/lessons/plot_UsingStatsbomb.py
@@ -1,91 +1,42 @@
-"""
-Using Statsbomb
-=====================
-Getting familiar with Statsbomb data
-"""
-
-#importing SBopen class from mplsoccer to open the data
+# Import the Sbopen class from the mplsoccer library to open the Statsbomb data
 from mplsoccer import Sbopen
-# The first thing we have to do is open the data. We use a parser SBopen available in mplsoccer.
+
+# Create a parser object using the Sbopen class to open the data
 parser = Sbopen()
 
-##############################################################################
-# Competition data
-# ----------------------------
-# Using method *competition* of the parser we can explore competitions to find the competition we are interested in.
-# The most important information for us is in the *competition_id* (id of competition) and *season_id*.
-# The first one is the key in Statsbomb database of a competition, the second one of a season 
-# of this competition (for example WC 2018 would have a different *season_id* than WC 2014, but the same *competition_id*).
-
-#opening data using competition method
+# Explore competition data to find the competition of interest, which is identified by competition_id and season_id.
+# The competition method returns a dataframe containing information about each competition, such as its name and ID.
 df_competition = parser.competition()
-#structure of data
+
+# Display information about the competition dataframe
 df_competition.info()
 
-
-
-##############################################################################
-# Match data
-# ----------------------------
-# Using method *match* of the parser we can explore matches of a competition to find the match we are interested in.
-# To open it we need to know the *competition_id* (id of competition) and *season_id*.
-# We know that for Women World Cup *competition_id* is 72 and *season_id* is 30
-# From this dataframe for us the most important imformation is provided in *match_id*, 
-# *home_team_id* and *home_team_name* and adequately *away_team_id* and *away_team_name*.
-
-#opening data using match method
+# Explore match data to find the match of interest, which is identified by competition_id and season_id.
+# The match method returns a dataframe containing information about each match, such as the teams that played and their IDs.
 df_match = parser.match(competition_id=72, season_id=30)
-#structure of data
+
+# Display information about the match dataframe
 df_match.info()
 
+# Explore lineup data to see the players who played in a specific match, identified by game_id.
+# The lineup method returns a dataframe containing information about each player who played, such as their team and jersey number.
+# NOTE: This section of code is currently commented out due to a change in data format.
+# df_lineup = parser.lineup(69301)
+# df_lineup.info()
 
-
-##############################################################################
-# Lineup data
-# ----------------------------
-# To check the lineups we use the *lineup* method. We do it for England Sweden WWC 2019 game - *game_id* is 69301 
-# - you can check that in the df_match. In this dataframe you will find all players who played in this game, their teams 
-# and jersey numbers
-# COMMENTED OUT BECAUSE OF CHANGE OF DATA FORMAT.
-
-#opening data using match method
-#df_lineup = parser.lineup(69301)
-#structure of data
-#df_lineup.info()
-
-##############################################################################
-# Event data
-# ----------------------------
-# The Statsbomb data that we will use the most during the course is event data. 
-# Knowing *game_id* you can open all the events that occured on the pitch
-# In the event dataframe you will find events with additional information, we will mostly use this dataframe.
-# Tactics dataframe provides information about player position on the pitch. 'Related' dataframe provides information
-# on events that were related to each other - for example ball pass and pressure applied. *df_freeze* consists of freezed
-# frames with player position in the moment of shots. We will learn more about tracking data later in the course.
-# Below, an example of event data is presented.
-
-#opening data
+# Explore event data to see the events that occurred in a specific match, identified by game_id.
+# The event method returns several dataframes, including one for the events themselves, one for related events, 
+# one for player tactics, and one for frozen frames with player position at the moment of shots.
 df_event, df_related, df_freeze, df_tactics = parser.event(69301)
-#if you want only event data you can use 
-#df_event = parser.event(69301)[0]
-#structure of data
 df_event.info()
 
-##############################################################################
-# 360 data
-# ----------------------------
-# Statsbomb offers 360 data which track not only location of an event but also players' location. To open them we need
-# an id of game. Later, we will also need id of the event. In the *df_frame* we find information on players' position (but only if teammate, not all information)
-# and in *df_visible* it is provided which part of the pitch was tracked during an event.
-
+# Explore 360 data, which includes the location of players during an event in addition to the location of the event itself.
+# The frame method returns a dataframe with information on player positions during the event, and the visible method 
+# returns information on the part of the pitch that was tracked during the event.
 df_frame, df_visible = parser.frame(3788741)
-
-# exploring the data
 df_frame.info()
 
+# Encourage the user to explore the dataframes before beginning any project or analysis
+# to become more familiar with the data.
 
-##############################################################################
-# Before you start
-# ----------------------------
-# Run these lines in Spyder/Jupyter notebook and explore dataframes 
-# to get more familiar before you start working on the course.
+
